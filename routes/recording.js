cd20f9c4bf824fe7a37b31c63e8712b182a8d3ae
Martin Lopez <lopez9@email.arizona.edu>
2021-10-27 21:21:38 +0000

    Upload for Lab6

---
+++ routes/recording.js
@@ -0,0 +1,49 @@
+var express = require('express');
+var router = express.Router();
+var Recording = require("../models/recording");
+
+router.get('/lab/status', function(req, res){
+
+    Recording.find({zip: req.body.zip}, {airQuality: req.body.airQuality}, function(err, docs){
+        if (zip == ""){
+            let errmsg = {"error" : "a zip code is required."};
+            res.status(400).send(JSON.stringify(errmsg));
+        }
+        else if(!zip){
+            let errmsg = {"error" : "Zip does not exist in the database."};
+            res.status(400).send(JSON.stringify(errmsg));
+        }
+        else{
+            const getAverageAirQuality = async(Recording, zip) => {
+                const average = await Recording.aggregate([
+                    {$match : {zip: req.body.zip}},
+                    {$group: {_id: null, average:{$avg: 'airQuality'}}},
+                ]).exec()
+                return average;
+            };
+            res.status(200).json(average);    
+        }
+    })
+});
+
+router.post('/lab/register', function(req,res){
+    const newRecording = new Recording({
+        zip: req.body.zip,
+        airQuality: req.body.airQuality
+    });
+
+    newRecording.save(function(err, recording){
+        if(err){
+            var errormsg = {"error" : "zip and airQuality are required."};
+            res.status(400).send(JSON.stringify(errormsg))
+            return;
+        }
+        else{
+            var msg = {"response" : "Data recorded."};
+            res.status(201).send(JSON.stringify(msg));
+            return; 
+        }
+    })
+})
+
+module.exports = router;
