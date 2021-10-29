23dc745c573d60a4a11bdee6b9b2510f964ea3c4
Martin Lopez <lopez9@email.arizona.edu>
2021-10-29 04:15:47 +0000

    updates to route

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var recordingRouter = require('./routes/lab');
+var labRouter = require('./routes/lab');
 
 var app = express();
 
@@ -22,7 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/lab', recordingRouter);
+app.use('/lab', labRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
